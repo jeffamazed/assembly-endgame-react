9be442b1f2f50074282790631a76ba204244b421
Jeffry <jeffry1408.job@gmail.com>
2025-05-19 15:20:52 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/assembly-endgame/",
+  base: "/assembly-endgame-react/",
   plugins: [react()],
 });
